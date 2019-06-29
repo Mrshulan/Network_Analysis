572ff22806ed882f90ad8bdb15a22b0ac95cb020
mrshulan <adengminjie@163.com>
2019-06-29 23:51:24 +0800

    init

---
+++ main.js
@@ -0,0 +1,50 @@
+// 引入eletron 创建一个BrowserWindow对象
+const { app, BrowserWindow } = require('electron');
+// const path = require('path');
+// const url = require('url');
+
+// 保持window对象的全局引用,避免JavaScript对象被垃圾回收了，造成窗口关闭
+let mainWindow;
+
+function createWindow () {
+  // 自定义窗口巴拉的
+  mainWindow = new BrowserWindow({width: 800, height: 600 });
+
+
+  // 加载应用----react 打包 electron-quick-start中默认的加载入口
+  // mainWindow.loadURL(url.format({
+  //   pathname: path.join(__dirname, './build/index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }))
+  // 加载应用----适用于 react:端口 开发时项目
+  // mainWindow.loadURL('http://localhost:3000/');
+
+  mainWindow.loadURL('http://localhost:3000/');
+
+  // 打开开发者工具，默认不打开
+  mainWindow.webContents.openDevTools();
+  // 关闭window时触发
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  })
+}
+
+// 当Electron完成初始化的时候 准备创建 浏览器窗口
+app.on('ready', createWindow)
+// 所有窗口关闭时退出应用
+app.on('window-all-closed', function () {
+  if(process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+
+// macOS中点击Dock图标时没有已打开的其余应用窗口时,则通常在应用中重建一个窗口
+app.on('activate', function() {
+  if(mainWindow === null) {
+    createWindow()
+  }
+})
+
+// 可以在这里引入其他的 js脚本
